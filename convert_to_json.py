08212905ec8278abcd80e977216a6f608c7d1aee
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-03-30 00:51:44 +0530

    chore: added minor hcnage

--- convert_to_json.py
+++ convert_to_json.py
@@ -7,6 +7,7 @@ import boto3
 from dotenv import load_dotenv
 from tqdm import tqdm
 import logging
+import xml.etree.ElementTree as ET
 
 CHUNK_SIZE = 8 * 1024 * 1024  # 8MB chunk size
 
@@ -14,34 +15,36 @@ CHUNK_SIZE = 8 * 1024 * 1024  # 8MB chunk size
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def csv_to_osm_xml(row):
+    way_id = int(row[0])  # Way id
+    node_ids_str = row[1].strip()  # Nodes
+    tags_str = row[2].strip()  # tags
 
-def csv_to_osm_json(row):
-    way_id = int(row[0]) # Way id
-    node_ids_str = row[1].strip() #  Nodes 
-    tags_str = row[2].strip() # tags 
-    
-    node_ids =[]
+    node_ids = []
     # Parse node IDs
-    if(node_ids_str):
+    if node_ids_str:
         input_node_string = node_ids_str.strip('{}')
-    # Split the string by commas and convert each element to an integer
+        # Split the string by commas and convert each element to an integer
         node_ids = [int(num) for num in input_node_string.split(',')]
+
     # Parse tags
     tags = {}
-    
-    if(tags_str and tags_str != ''):
-       tag_json = json.loads(tags_str)
-       tags=tag_json
-       
-    way = {
-        "type": "way",
-        "id": way_id,
-        "nodes": node_ids,
-        "tags": tags
-    }
-    return way
+    if tags_str and tags_str != '':
+        tag_json = json.loads(tags_str)
+        tags = tag_json
 
-def write_json_to_s3(json_data, bucket_name, file_key):
+    # Construct XML
+    way_element = ET.Element("way", attrib={"id": str(way_id)})
+    for node_id in node_ids:
+        node_element = ET.SubElement(way_element, "nd", attrib={"ref": str(node_id)})
+    for key, value in tags.items():
+        tag_element = ET.SubElement(way_element, "tag", attrib={"k": key, "v": value})
+
+    xml_string = ET.tostring(way_element, encoding='utf8', method='xml').decode()
+    print(f"\n\n{xml_string}\n\n")
+    return xml_string
+
+def write_xml_to_s3(xml_data, bucket_name, file_key):
     load_dotenv()
     aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -50,34 +53,30 @@ def write_json_to_s3(json_data, bucket_name, file_key):
         return
 
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-    upload_large_object(s3, json.dumps(json_data).encode('utf-8'), bucket_name, file_key)
+    upload_large_object(s3, xml_data.encode('utf-8'), bucket_name, file_key)
 
-def convert_csv_to_osm_json_and_write_to_s3(bucket_name, csv_key, json_key):
+def convert_csv_to_osm_xml_and_write_to_s3(bucket_name, csv_key, xml_key):
     load_dotenv()
     aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
     if not aws_access_key_id or not aws_secret_access_key:
         logger.error("AWS credentials are not set properly.")
         return
-    
-    osm_data = {"version": 0.6, "generator": "osmium/1.16.0"}
-    osm_data["elements"] = []
 
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     try:
         with s3.get_object(Bucket='touring-buddy', Key=csv_key)['Body'] as source_file:
             next(source_file)  # Skip the header line
-        # Process each line in the CSV
+            osm_xml_data = '<?xml version="1.0" encoding="UTF-8"?>\n<osm version="0.6" generator="osmium/1.16.0">\n'
             for line_num, row in tqdm(enumerate(csv.reader((line.decode('utf-8') for line in source_file))), desc="Processing Ways"):
-                json = csv_to_osm_json(row=row)
-                osm_data["elements"].append(json)
-                
-        write_json_to_s3(osm_data, bucket_name, json_key)
+                xml = csv_to_osm_xml(row=row)
+                osm_xml_data += xml + '\n'
+            osm_xml_data += '</osm>'
+        write_xml_to_s3(osm_xml_data, bucket_name, xml_key)
     except Exception as e:
-        print(f"LINE NUMBER : {line_num}",row)
+        print(f"LINE NUMBER : {line_num}", row)
         logger.error(f"Error processing CSV file: {e}")
-
 
 def upload_large_object(s3_client, body, bucket, key):
     # Initialize multipart upload
@@ -110,9 +109,8 @@ def upload_large_object(s3_client, body, bucket, key):
     except Exception as e:
         # Abort multipart upload on failure
         s3_client.abort_multipart_upload(Bucket=bucket, Key=key, UploadId=upload_id)
-        logger.error(f"Error uploading object to S3: {e}")
+        print(f"Error uploading object to S3: {e}")
         raise e
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script description")
@@ -122,5 +120,5 @@ if __name__ == "__main__":
     logger.info(f"STARTING FILE PROCESSING {args.source_file_name} {args.destination_file_name}")
     bucket_name = 'touring-buddy'
     csv_file_key = args.source_file_name
-    json_file_key = args.destination_file_name
-    convert_csv_to_osm_json_and_write_to_s3(bucket_name, csv_file_key, json_file_key)
+    xml_file_key = args.destination_file_name
+    convert_csv_to_osm_xml_and_write_to_s3(bucket_name, csv_file_key, xml_file_key)
